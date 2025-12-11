09a136d46e814aac9e4cffdcb27f28e7c56c2ee5
Mehdi Samardan <mehdisamardanjr@gmail.com>
2025-12-11 18:04:46 +0100

    .

--- src/app/lib/redis.ts
+++ src/app/lib/redis.ts
@@ -2,3 +2,9 @@ import { Redis } from "@upstash/redis";
 
 // Create a Redis client using environment variables - automatically picks up UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN
 export const redis = Redis.fromEnv();
+
+// we can also pass them manually like below
+// export const redis = new Redis({
+//   url: process.env.UPSTASH_REDIS_REST_URL || "",
+//   token: process.env.UPSTASH_REDIS_REST_TOKEN || "",
+// });
